7ac72ed5086d8556c570c548a293fa0de612fc03
dovmf <dovmf@218.52.202.180>
2022-04-23 19:06:34 +0900

    second

--- src/OkjavaGoinHome_input.java
+++ src/OkjavaGoinHome_input.java
@@ -1,15 +1,22 @@
 
 
+import javax.swing.JOptionPane;
+
 import org.opentutorials.iot.Elevator;
 import org.opentutorials.iot.Security;
 import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.DimmingLights;
 
-public class OkjavaGoinHome {
+public class OkjavaGoinHome_input {
+									//args = parameter or 매개변수라고 불리우며 일종이 변수로 인식/배열형태이며 
+									// run configuration에서 arguments 값을 받아다 넣음(변수가 배열형이기에 다수 값 받을 수)
 	public static void main(String[] args) {
 		//org라는 패키지 안에있는 java application 활용하여 
 	
-		String id  = "JAVA APT 507";
-		
+//		String id  = JOptionPane.showInputDialog("Enter a ID"); //CTRL SHIFT M
+//		String bright  = JOptionPane.showInputDialog("Enter a bright"); //CTRL SHIFT M
+		String id = args[0];
+		String bright = args[1];
 		
 		//Elevator call
 		Elevator myElevator = new Elevator(id);
@@ -30,7 +37,9 @@ public class OkjavaGoinHome {
 		//디버거에서 step over - 한줄한줄 내려가면서 실행
 		// step into - 메서드사용하고 있다면 해당 메서드로 들어가기  되돌아가려면 step return 
 	
-	
+		DimmingLights moodLamp = new DimmingLights(id + " / moodLamp");
+		moodLamp.setBright(Double.parseDouble(bright)); //위에서 입력받은 값은 string이기에 입력칸에서 double로 전환 
+		moodLamp.on();
 	
 	}
 
--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,29 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		System.out.println("abc\n"+"dne");
 		
+		System.out.println(Math.PI);
+		//1회용 작업의 경우 인스턴스필요없으나 (생성자-constructor가 없는 경우)
+		
+		PrintWriter p1 = new PrintWriter("result1.exe");
+	   //데이터타입  변수명   클래스명에 new 를 붙임으로써 인스턴스생성 
+		p1.write("Hello 1"); //인스턴스.쓰고자하는기능 (PrintWriter 클래스의 write 기능)
+		p1.close();
+
+		PrintWriter p2 = new PrintWriter("result2.exe");
+		p2.write("Hello 2");
+		p2.close();
+		
+		//다수파일작업이나 수정이 필요한 작업의 경우 new를 붙여 클래스를 복제하여 각기 다른 상태를 가진 인스턴스로 만들어 사용하는 것이 효율적 
+		//예시
+		Scanner apk = new Scanner(System.in);
+		String a = apk.nextLine();
+		System.out.println(a);
 	}
 
 }
